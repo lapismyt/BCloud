23806e0ae8d6f58213176f9dd58819da99981ae9
LapisMYT <nikitagavrilin005@gmail.com>
2023-08-10 20:56:45 +0300

    .

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import json
 import random
 import os, sys
 import pickle
-import urllib.parse
+from flaskext.markdown import Markdown
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 AUTHTOKEN = os.environ.get("BCLOUD_AUTHTOKEN")
 print(f"Current authtoken: {AUTHTOKEN}")
+here = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def index():
@@ -21,12 +23,12 @@ def newpost():
 @app.route("/publish", methods=["POST"])
 def publish():
     if request.form["authtoken"] == os.environ.get("BCLOUD_AUTHTOKEN"):
-        posts_db = pickle.load(open("data/posts.pkl", "rb"))
+        posts_db = json.load(open("data/posts.json"))
         post_id = str(random.randint(10000, 99999))
         post = {"header": request.form["header"], "body": request.form["body"], "id": post_id}
         print(post)
-        posts_db.add(post)
-        pickle.dump(posts_db, open("data/posts.pkl", "wb"))
+        posts_db[post_id] = post
+        json.dump(posts_db, open("data/posts.json", "w"))
         if not ("posts" in session):
             session["posts"] = []
         session["posts"].append(post_id)
@@ -35,24 +37,17 @@ def publish():
         return "Неверный ключ", 403
 
 @app.route("/posts/<post_id>")
-def posts(post_id):
-    posts_db = pickle.load(open("data/posts.pkl", "rb"))
+def posts(post_id): 
+    posts_db = json.load(open("data/posts.json"))
     if posts_db.get(post_id) is not None:
         post = posts_db.get(post_id)
-        return render_template("post.html", header=post["header"], body=post["body"])
+        res = render_template("post.html", header=post["header"], body=post["body"])
+        return res
     else:
-        return "Not Found", 404
+        return "<h1>Not Found</h1>", 404
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
         app.run(debug=False, host=sys.argv[1], port=sys.argv[2])
     else:
         app.run(debug=False)
-
-@app.route("/admin/api/{token}/{do}", methods=["GET"])
-def admin_features(token, do):
-    tokens = json.load(open("data/api_tokens.json"))
-    if token not in tokens:
-        return 403
-    else:
-        return 403
